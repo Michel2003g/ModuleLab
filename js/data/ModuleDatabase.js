6f16b8fce040b0274d999558ffcfa7de6b96829a
Michel2003g <111076051+Michel2003g@users.noreply.github.com>
2023-01-07 06:15:54 +0100

    update

--- js/data/ModuleDatabase.js
+++ js/data/ModuleDatabase.js
@@ -8,12 +8,6 @@ class Module {
     }
 }
 
-const DocTypes = {
-    Explainer: "Explainer",
-    Video: "Video",
-    Codeblock: "Codeblock"
-}
-
 const defaultExport = {
     "MeshParticles": new Module(
         "3D Particle System",
@@ -22,6 +16,21 @@ const defaultExport = {
         "This Module allows you to spawn meshes and models from a particle system.",
         [
 
+        ]
+    ),
+    "OptiLoop": new Module(
+        "Opti Loop",
+        "OptiLoop",
+        "/ModuleLab/img/icons/script-icon.svg",
+        "Make use of many loops without a performance hit. (depending on code within the loop)",
+        [
+            "<p>Sometime ago i noticed that calling multiple for/while loops can have a massive impact on performane in game." + 
+            "But sometimes you really need those loops." +
+            "My Solution: Opti Loop." +
+            "Opti Loop uses Runservice and its stepped event to do the same as loops only with way less performance issues." +
+            "On a test run i could run 100 loops and still have no issues with performance." +
+            "This ofcourse can be different for you depending on what code you're running in those loops." +
+            "</p>"
         ]
     ),
     };
